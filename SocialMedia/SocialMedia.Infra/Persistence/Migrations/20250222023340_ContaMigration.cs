f27d96785ceb14a30457b76ac304fae83069447a
Marcos Paulo <marcos.poo@gmail.com>
2025-02-21 23:35:59 -0300

    feat: Adiciona Migration para a entidade Conta.

---
+++ SocialMedia/SocialMedia.Infra/Persistence/Migrations/20250222023340_ContaMigration.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialMedia.Infra.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ContaMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Conta",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeCompleto = table.Column<string>(type: "varchar(150)", nullable: false),
+                    Senha = table.Column<string>(type: "varchar(15)", nullable: false),
+                    Email = table.Column<string>(type: "varchar(150)", nullable: false),
+                    Telefone = table.Column<string>(type: "varchar(20)", nullable: false),
+                    DataNascimento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Conta", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Conta_Email",
+                table: "Conta",
+                column: "Email");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Conta");
+        }
+    }
+}

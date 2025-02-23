7e7ccd13bb9511ce3ff5c9babc99c298f35e5fd0
Marcos Paulo <marcos.poo@gmail.com>
2025-02-22 22:23:04 -0300

    feat: Adicionando classes de Mensagens.

---
+++ SocialMedia/SocialMedia.Core/Messages/BaseMsgs.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Resources;
+
+namespace SocialMedia.Core.Messages
+{
+    public static class BaseMsgs
+    {
+        public static string GetResource(string key, string FileNameResx)
+        {
+            var rm = new ResourceManager(FileNameResx, typeof(BaseMsgs).Assembly);
+
+            var text = rm.GetString(key, CultureInfo.CurrentCulture);
+
+            if (text != null)
+                return text;
+            else
+                return "null";
+        }
+    }
+}
---
+++ SocialMedia/SocialMedia.Core/Messages/ContaMessages/ContaMsgs.cs
@@ -0,0 +1,71 @@
+﻿namespace SocialMedia.Core.Messages.ContaMessages
+{
+    public static class ContaMsgs
+    {
+        private const string fileNameResx = "SocialMedia.Core.Messages.ContaMessages.ContaMsgsResource";
+
+        public static string GetContaNotExist()
+        {
+            return BaseMsgs.GetResource("ContaNotExist", fileNameResx);
+        }
+
+        public static string GetNomeCompletoNotEmpty()
+        {
+            return BaseMsgs.GetResource("ContaNomeCompletoNotEmpty", fileNameResx);
+        }
+
+        public static string GetNomeCompletoMaxLength()
+        {
+            return BaseMsgs.GetResource("ContaNomeCompletoMaxLength", fileNameResx);
+        }
+
+        public static string GetSenhaNotEmpty()
+        {
+            return BaseMsgs.GetResource("ContaSenhaNotEmpty", fileNameResx);
+        }
+
+        public static string GetSenhaMaxLength()
+        {
+            return BaseMsgs.GetResource("ContaSenhaMaxLength", fileNameResx);
+        }
+        public static string GetSenhaInvalid()
+        {
+            return BaseMsgs.GetResource("ContaSenhaInvalid", fileNameResx);
+        }
+
+        public static string GetEmailNotEmpty()
+        {
+            return BaseMsgs.GetResource("ContaEmailNotEmpty", fileNameResx);
+        }
+
+        public static string GetEmailMaxLength()
+        {
+            return BaseMsgs.GetResource("ContaEmailMaxLength", fileNameResx);
+        }
+
+        public static string GetEmailInvalid()
+        {
+            return BaseMsgs.GetResource("ContaEmailInvalid", fileNameResx);
+        }
+        
+        public static string GetTelefoneNotEmpty()
+        {
+            return BaseMsgs.GetResource("ContaTelefoneNotEmpty", fileNameResx);
+        }
+
+        public static string GetTelefoneMaxLength()
+        {
+            return BaseMsgs.GetResource("ContaTelefoneMaxLength", fileNameResx);
+        }
+
+        public static string GetDataNascimentoNotEmpty()
+        {
+            return BaseMsgs.GetResource("ContaDataNascimentoNotEmpty", fileNameResx);
+        }
+
+        public static string GetDataNascimentoMinAge()
+        {
+            return BaseMsgs.GetResource("ContaDataNascimentoMinAge", fileNameResx);
+        }
+    }
+}

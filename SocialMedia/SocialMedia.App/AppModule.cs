a2d685bf29a95dd6677863fd4b005d863448c3fe
Marcos Paulo <marcos.poo@gmail.com>
2025-02-22 22:37:28 -0300

    feat: Adiciona a classe AppModule ao projeto App.

---
+++ SocialMedia/SocialMedia.App/AppModule.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
+using SocialMedia.App.Models.Contas;
+using SocialMedia.App.Services.Contas;
+
+namespace SocialMedia.App
+{
+    public static class AppModule
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services
+                .AddServices()
+                .AddValidation();
+
+            return services;
+        }
+
+        private static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<IContaService, ContaService>();
+
+            return services;
+        }
+
+        private static IServiceCollection AddValidation(this IServiceCollection services)
+        {
+            services
+                .AddFluentValidationAutoValidation()
+                .AddValidatorsFromAssemblyContaining<CreateContaInputModel>();
+
+            return services;
+        }
+    }
+}

3b621da5eaa6ae206d3cfdfae167b7b16d09bfd7
Marcos Paulo <marcos.poo@gmail.com>
2025-02-22 22:40:38 -0300

    feat: Adiciona classe de Validator para a classe UpdateConta.

---
+++ SocialMedia/SocialMedia.App/Validators/ContaValidator/UpdateContaInputModelValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FluentValidation;
+using SocialMedia.App.Models.Contas;
+using SocialMedia.Core.Messages.ContaMessages;
+
+namespace SocialMedia.App.Validators.ContaValidator
+{
+    public class UpdateContaInputModelValidator : AbstractValidator<UpdateContaInputModel>
+    {
+        public UpdateContaInputModelValidator()
+        {
+            RuleFor(c => c.NomeCompleto)
+                .MaximumLength(150)
+                    .WithMessage(ContaMsgs.GetNomeCompletoMaxLength());
+
+            RuleFor(c => c.DataNascimento)
+                .Must(d => d < DateTime.Now.AddYears(-18))
+                    .WithMessage(ContaMsgs.GetDataNascimentoMinAge());
+        }
+    }
+}

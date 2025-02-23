f8a02ad166185dd3e04986def32e38ae0589e794
Marcos Paulo <marcos.poo@gmail.com>
2025-02-23 01:05:50 -0300

    refactor: Alteração no nome da variável.

--- SocialMedia/SocialMedia.Core/Messages/BaseMsgs.cs
+++ SocialMedia/SocialMedia.Core/Messages/BaseMsgs.cs
@@ -5,9 +5,9 @@ namespace SocialMedia.Core.Messages
 {
     public static class BaseMsgs
     {
-        public static string GetResource(string key, string FileNameResx)
+        public static string GetResource(string key, string fileNameResx)
         {
-            var rm = new ResourceManager(FileNameResx, typeof(BaseMsgs).Assembly);
+            var rm = new ResourceManager(fileNameResx, typeof(BaseMsgs).Assembly);
 
             var text = rm.GetString(key, CultureInfo.CurrentCulture);
 

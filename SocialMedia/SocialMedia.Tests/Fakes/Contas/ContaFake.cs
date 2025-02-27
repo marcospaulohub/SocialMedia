7c599d5f4924ebdda0e7388c10e60acfaaf4dab5
Marcos Paulo <marcos.poo@gmail.com>
2025-02-26 21:23:56 -0300

    test: Cria objetos com dados Fake utilizando a biblioteca Bogus.

---
+++ SocialMedia/SocialMedia.Tests/Fakes/Contas/ContaFake.cs
@@ -0,0 +1,22 @@
+﻿using Bogus;
+using SocialMedia.Core.Entities;
+using System;
+
+namespace SocialMedia.Tests.Fakes.Contas
+{
+    public class ContaFake : Faker<Conta>
+    {
+        public ContaFake()
+        {
+            CustomInstantiator(faker =>
+            new Conta(
+                faker.Name.FullName(),
+                faker.Internet.Password(10),
+                faker.Internet.Email(),
+                faker.Phone.PhoneNumber(),
+                DateTime.Now.AddYears(-20)
+                )
+            );
+        }
+    }
+}
---
+++ SocialMedia/SocialMedia.Tests/Fakes/Contas/UpdateContaInputModelFake.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Bogus;
+using SocialMedia.App.Models.Contas;
+
+namespace SocialMedia.Tests.Fakes.Contas
+{
+    public class UpdateContaInputModelFake : Faker<UpdateContaInputModel>
+    {
+        public UpdateContaInputModelFake()
+        {
+            CustomInstantiator(faker =>
+            new UpdateContaInputModel(
+                faker.Name.FullName(),
+                DateTime.Now.AddYears(-20)
+                )
+            );
+        }
+    }
+}

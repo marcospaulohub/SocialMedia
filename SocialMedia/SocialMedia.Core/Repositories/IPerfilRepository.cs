a4cb2d072e1a43a8ba0f6a681459a2aada708221
Marcos Paulo <marcos.poo@gmail.com>
2025-03-07 22:03:21 -0300

    feat: Adiciona a interface IPerfilRepository.

---
+++ SocialMedia/SocialMedia.Core/Repositories/IPerfilRepository.cs
@@ -0,0 +1,13 @@
+﻿using SocialMedia.Core.Entities;
+using System.Collections.Generic;
+
+namespace SocialMedia.Core.Repositories
+{
+    public interface IPerfilRepository
+    {
+        void Update(Perfil perfil);
+        Perfil? GetById(int id);
+        List<Perfil>? GetAll(int idConta);
+        void Delete(Perfil perfil);
+    }
+}

a1471ea7b8bffadbe9862d8d164c91ede409fc8a
Marcos Paulo <marcos.poo@gmail.com>
2025-03-07 22:01:33 -0300

    feat: Adiciona a entidade Perfil.

---
+++ SocialMedia/SocialMedia.Core/Entities/Perfil.cs
@@ -0,0 +1,35 @@
+﻿namespace SocialMedia.Core.Entities
+{
+    public class Perfil: BaseEntity
+    {
+        public Perfil() { }
+        public Perfil(int idConta, Conta conta, string nomeExibicao, string sobre, string foto, string localidade, string profissao)
+            : base()
+        {
+            IdConta = idConta;
+            Conta = conta;
+            NomeExibicao = nomeExibicao;
+            Sobre = sobre;
+            Foto = foto;
+            Localidade = localidade;
+            Profissao = profissao;
+        }
+
+        public int IdConta { get; private set; }
+        public Conta Conta { get; private set; }
+        public string NomeExibicao { get; private set; }
+        public string Sobre { get; private set; }
+        public string Foto { get; private set; }
+        public string Localidade { get; private set; }
+        public string Profissao { get; private set; }
+
+        public void Update(string nomeExibicao, string sobre, string foto, string localidade, string profissao)
+        {
+            NomeExibicao = nomeExibicao;
+            Sobre = sobre;
+            Foto = foto;
+            Localidade = localidade;
+            Profissao = profissao;
+        }
+    }
+}

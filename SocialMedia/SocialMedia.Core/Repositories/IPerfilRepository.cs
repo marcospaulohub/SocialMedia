9bf60ef909fb7e49844f17a9e3b7271fd7f3f906
Marcos Paulo <marcos.poo@gmail.com>
2025-03-08 20:54:52 -0300

    feat: Adiciona o método Insert.

--- SocialMedia/SocialMedia.Core/Repositories/IPerfilRepository.cs
+++ SocialMedia/SocialMedia.Core/Repositories/IPerfilRepository.cs
@@ -5,6 +5,7 @@ namespace SocialMedia.Core.Repositories
 {
     public interface IPerfilRepository
     {
+        int Insert(Perfil perfil);
         void Update(Perfil perfil);
         Perfil? GetById(int id);
         List<Perfil>? GetAll(int idConta);

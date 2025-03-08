495054b48f1ae5e85da979ec69958e6b51fcc311
Marcos Paulo <marcos.poo@gmail.com>
2025-03-08 20:26:41 -0300

    feat: Adiciona a Migration da classe Perfil.

--- SocialMedia/SocialMedia.Infra/Persistence/Migrations/20250308231605_PerfilMigration.Designer.cs
+++ SocialMedia/SocialMedia.Infra/Persistence/Migrations/20250308231605_PerfilMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SocialMedia.Infra.Persistence;
 
@@ -11,9 +12,11 @@ using SocialMedia.Infra.Persistence;
 namespace SocialMedia.Infra.Persistence.Migrations
 {
     [DbContext(typeof(SocialMediaDbContext))]
-    partial class SocialMediaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250308231605_PerfilMigration")]
+    partial class PerfilMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SocialMedia/SocialMedia.Infra/Persistence/Migrations/20250308231605_PerfilMigration.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialMedia.Infra.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class PerfilMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Perfil",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdConta = table.Column<int>(type: "int", nullable: false),
+                    NomeExibicao = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Sobre = table.Column<string>(type: "varchar(max)", nullable: false),
+                    Foto = table.Column<string>(type: "varchar(max)", nullable: false),
+                    Localidade = table.Column<string>(type: "varchar(20)", nullable: false),
+                    Profissao = table.Column<string>(type: "varchar(150)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Perfil", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Perfil_Conta_IdConta",
+                        column: x => x.IdConta,
+                        principalTable: "Conta",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Perfil_IdConta",
+                table: "Perfil",
+                column: "IdConta");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Perfil");
+        }
+    }
+}

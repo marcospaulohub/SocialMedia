b62afe48639f2d005bcb65205eea44eed199b6df
Marcos Paulo <marcos.poo@gmail.com>
2025-05-30 16:20:52 -0300

    refactor: Removendo construtor primário.

--- SocialMedia/SocialMedia.Core/Entities/Perfil.cs
+++ SocialMedia/SocialMedia.Core/Entities/Perfil.cs
@@ -1,14 +1,30 @@
 ﻿namespace SocialMedia.Core.Entities
 {
-    public class Perfil(int idConta, Conta conta, string nomeExibicao, string sobre, string foto, string localidade, string profissao) : BaseEntity()
+    public class Perfil : BaseEntity
     {
-        public int IdConta { get; private set; } = idConta;
-        public Conta Conta { get; private set; } = conta;
-        public string NomeExibicao { get; private set; } = nomeExibicao;
-        public string Sobre { get; private set; } = sobre;
-        public string Foto { get; private set; } = foto;
-        public string Localidade { get; private set; } = localidade;
-        public string Profissao { get; private set; } = profissao;
+        public int IdConta { get; private set; }
+        public Conta Conta { get; private set; }
+        public string NomeExibicao { get; private set; }
+        public string Sobre { get; private set; }
+        public string Foto { get; private set; }
+        public string Localidade { get; private set; }
+        public string Profissao { get; private set; }
+
+        public Perfil()
+        {
+            
+        }
+
+        public Perfil(int idConta, Conta conta, string nomeExibicao, string sobre, string foto, string localidade, string profissao) : base()
+        {
+            IdConta = idConta;
+            Conta = conta;
+            NomeExibicao = nomeExibicao;
+            Sobre = sobre;
+            Foto = foto;
+            Localidade = localidade;
+            Profissao = profissao;
+        }
 
         public void Update(string nomeExibicao, string sobre, string foto, string localidade, string profissao)
         {
